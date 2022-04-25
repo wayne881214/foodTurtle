bc3573b6b1e2b32ce2011e5d0e74e141feb827ea
bobo <pauleric789456@gmail.com>
2022-04-25 12:11:02 +0800

    bobo_deliveryUI_map

--- app/src/main/java/fcu/app/foodturtle/MainActivity.java
+++ app/src/main/java/fcu/app/foodturtle/MainActivity.java
@@ -2,7 +2,9 @@ package fcu.app.foodturtle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    protected void gonext(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, deliveryOrderActivity.class);
+        startActivity(intent);
+    }
+
+    protected void god(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, NavigationActivity.class);
+        startActivity(intent);
     }
 }
