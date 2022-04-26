4f6dc071937960e03407a2f8cb6cbe7afbc1b950
peter <abcde12345326@gmail.com>
2022-04-26 18:37:57 +0800

    peter_browseUI_create

--- app/src/main/java/fcu/app/foodturtle/MainActivity.java
+++ app/src/main/java/fcu/app/foodturtle/MainActivity.java
@@ -2,13 +2,21 @@ package fcu.app.foodturtle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		if (getSupportActionBar() != null){
+			getSupportActionBar().hide();
+		}
+//		setContentView(R.layout.activity_main);
+			setContentView(R.layout.activity_browse);
+
 	}
+
 }
