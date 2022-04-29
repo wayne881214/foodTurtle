2845daf7ea1925f07c49907b015895e05cde57c0
peter <abcde12345326@gmail.com>
2022-04-29 16:28:03 +0800

    peter_shop_car2UI

--- app/src/main/java/fcu/app/foodturtle/MainActivity.java
+++ app/src/main/java/fcu/app/foodturtle/MainActivity.java
@@ -3,13 +3,22 @@ package fcu.app.foodturtle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	public void homePage(View v) {
+		setContentView(R.layout.activity_main);
+	}
+	public void onClick1(View v) {
 		setContentView(R.layout.activity_shop_car);
-//		setContentView(R.layout.activity_main);
+	}
+	public void onClick2(View v) {
+		setContentView(R.layout.activity_shop_car_2);
 	}
 }
