7f673bc8a6fa67a5384432eb4b06627d6f1a91b8
peter <abcde12345326@gmail.com>
2022-05-01 17:41:38 +0800

    peter_couponUI_init

--- app/src/main/java/fcu/app/foodturtle/MainActivity.java
+++ app/src/main/java/fcu/app/foodturtle/MainActivity.java
@@ -3,12 +3,22 @@ package fcu.app.foodturtle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_now);
+		setContentView(R.layout.activity_main);
+	}
+	public void home(View v) {
+		setContentView(R.layout.activity_main);
+	}
+	public void onClick1(View v) {
+		setContentView(R.layout.activity_coupon_now);
+	}
+	public void onClick2(View v) {
+		setContentView(R.layout.activity_coupon_expired);
 	}
 }
