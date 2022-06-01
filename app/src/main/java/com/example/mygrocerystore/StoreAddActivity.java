3864fe4620e286ec967ae1280b18c62cdabc5647
peter <abcde12345326@gmail.com>
2022-06-02 01:46:26 +0800

    addStore

---
+++ app/src/main/java/com/example/mygrocerystore/StoreAddActivity.java
@@ -0,0 +1,14 @@
+package com.example.mygrocerystore;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreAddActivity extends AppCompatActivity  {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_store_add);
+
+	}
+
+}
