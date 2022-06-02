6e27f184e548b5867af6c10b85ea6a3756e98b91
peter <abcde12345326@gmail.com>
2022-06-02 23:24:11 +0800

    peter-temp0602

--- app/src/main/java/com/example/mygrocerystore/AddStoreActivity.java
+++ app/src/main/java/com/example/mygrocerystore/AddStoreActivity.java
@@ -1,4 +1,5 @@
 package com.example.mygrocerystore;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -6,10 +7,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mygrocerystore.models.PopularModel;
+import com.example.mygrocerystore.models.UserModel;
 import com.example.mygrocerystore.models.ViewAllModel;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -32,12 +37,12 @@ public class AddStoreActivity extends AppCompatActivity {
 	private EditText etFoodImg;
 
 	private FirebaseAuth firebaseAuth;
-
+	String storename="";
+	String FoodType="";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_store_add);
-
 		etName = findViewById(R.id.storeName1);
 		etFreight = findViewById(R.id.storeFreight1);
 		etFraction = findViewById(R.id.storeFraction1);
@@ -57,6 +62,7 @@ public class AddStoreActivity extends AppCompatActivity {
 	public void addStore(View view) {
 
 		String Name = etName.getText().toString();
+		String storeName = etName.getText().toString();
 		String Freight = etFreight.getText().toString();
 		String Fraction = etFraction.getText().toString();
 		String Type = etType.getText().toString();
@@ -66,13 +72,12 @@ public class AddStoreActivity extends AppCompatActivity {
 		String img_url="https://firebasestorage.googleapis.com/v0/b/my-grocery-store-60a2c.appspot.com/o/grocery10.jpg?alt=media&token=4de96ce4-cd4f-44e6-9513-56e83944bd91";
 
 		FirebaseFirestore firebaseDatabase = FirebaseFirestore.getInstance();
-		PopularModel store =new PopularModel(Name,Des,Fraction,Freight,Name,Type,img_url);
+		PopularModel store =new PopularModel(Name,Des,Fraction,Freight,storeName,Type,img_url);
 		firebaseDatabase.collection("PopularProducts").document(Name).set(store);
-
 		//Real time
 		FirebaseDatabase firebaseDatabase2 = FirebaseDatabase.getInstance();
 		DatabaseReference storesRef = firebaseDatabase2.getReference("Stores");
-		DatabaseReference nameRef = storesRef.child(Name);
+		DatabaseReference nameRef = storesRef.child(FirebaseAuth.getInstance().getUid());
 		Map<String, Object> store2 = new HashMap<>();
 		store2.put("storeName", Name);
 		store2.put("type", Type);
@@ -85,15 +90,41 @@ public class AddStoreActivity extends AppCompatActivity {
 		String Name = etName.getText().toString();
 		String FoodName = etFoodName.getText().toString();
 		String FoodCommit = etFoodCommit.getText().toString();
-		String FoodType = etFoodType.getText().toString();
+
 		String FoodMoney = etFoodMoney.getText().toString();
 		String FoodRate = etFoodRate.getText().toString();
 		String FoodImg = etFoodImg.getText().toString();
 
+		FirebaseDatabase database;
+		database = FirebaseDatabase.getInstance();
+		database.getReference().child("Stores").child(FirebaseAuth.getInstance().getUid()).child("storeName")
+		.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull DataSnapshot snapshot) {
+				storename=snapshot.getValue(String.class);
+			}
+
+			@Override
+			public void onCancelled(@NonNull DatabaseError error) {
+
+			}
+		});
+		database.getReference().child("Stores").child(FirebaseAuth.getInstance().getUid()).child("type")
+		.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull DataSnapshot snapshot) {
+				FoodType=snapshot.getValue(String.class);
+			}
+
+			@Override
+			public void onCancelled(@NonNull DatabaseError error) {
+
+			}
+		});
 		FirebaseFirestore firebaseDatabase = FirebaseFirestore.getInstance();
 		String img_url="https://firebasestorage.googleapis.com/v0/b/my-grocery-store-60a2c.appspot.com/o/grocery10.jpg?alt=media&token=4de96ce4-cd4f-44e6-9513-56e83944bd91";
 
-		ViewAllModel food=new ViewAllModel(FoodName,FoodCommit,FoodRate,Name,FoodType,img_url,Integer.parseInt(FoodMoney));
+		ViewAllModel food=new ViewAllModel(FoodName,FoodCommit,FoodRate,storename,FoodType,img_url,Integer.parseInt(FoodMoney));
 		firebaseDatabase.collection("AllProducts").document(FoodName).set(food);
 
 //		PopularModel store =new PopularModel(Name,Fraction,Name,Name,Freight,Type);
--- app/src/main/java/com/example/mygrocerystore/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/mygrocerystore/ui/slideshow/SlideshowFragment.java
@@ -1,28 +1,40 @@
 package com.example.mygrocerystore.ui.slideshow;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mygrocerystore.AddStoreActivity;
+import com.example.mygrocerystore.R;
 import com.example.mygrocerystore.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
+	Activity activity;
+	Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-
+			View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+			activity = getActivity();
+			button = root.findViewById(R.id.button);
+			button.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					Intent intent = new Intent(activity, AddStoreActivity.class);
+					startActivity(intent);
+				}
+			});
         return root;
     }
 
