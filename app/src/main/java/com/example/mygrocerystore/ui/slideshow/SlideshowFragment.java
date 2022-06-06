188282872b6f392c143258598b8e1069e1423539
peter <abcde12345326@gmail.com>
2022-06-06 22:16:01 +0800

    peter-orderShow

--- app/src/main/java/com/example/mygrocerystore/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/mygrocerystore/ui/slideshow/SlideshowFragment.java
@@ -12,18 +12,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mygrocerystore.AddStoreActivity;
 import com.example.mygrocerystore.R;
 import com.example.mygrocerystore.StoreOrderActivity;
+import com.example.mygrocerystore.activities.OrderDetail;
+import com.example.mygrocerystore.adapter.MyCartAdapter;
 import com.example.mygrocerystore.databinding.FragmentSlideshowBinding;
 import com.example.mygrocerystore.models.FullOrderModel;
+import com.example.mygrocerystore.models.MyCartModel;
 import com.example.mygrocerystore.models.OrderModel;
+import com.example.mygrocerystore.models.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SlideshowFragment extends Fragment {
 
@@ -31,61 +42,101 @@ public class SlideshowFragment extends Fragment {
 	Activity activity;
 	Button button;
 	Button testOrder;
+	String storeName="null";
+	List<MyCartModel> cartModelList,cartModelList2,cartModelList3;
+	MyCartAdapter cartAdapter,cartAdapter2,cartAdapter3;
+	RecyclerView recyclerView,recyclerView2,recyclerView3;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+
+
+	public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
 
 			View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 			activity = getActivity();
-			button = root.findViewById(R.id.button);
-			button.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					Intent intent = new Intent(activity, AddStoreActivity.class);
-					startActivity(intent);
-				}
-			});
 
-			testOrder = root.findViewById(R.id.waitAceptOrder);
-			testOrder.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					Intent intent = new Intent(activity, StoreOrderActivity.class);
-					startActivity(intent);
-				}
-			});
+			TextView store=root.findViewById(R.id.storeName);
 
 			FirebaseDatabase database = FirebaseDatabase.getInstance();
-			DatabaseReference ordersRef = database.getReference("/Orders/");
+			database.getReference().child("Stores").child(FirebaseAuth.getInstance().getUid()).child("storeName")
+			.addValueEventListener(new ValueEventListener() {
+				@Override
+				public void onDataChange(@NonNull DataSnapshot snapshot) {
+					storeName=snapshot.getValue(String.class);
+					store.setText("商店名稱:"+storeName);
+				}
 
-			//讀取商家 寫死中
-			String storeName="123";
-			TextView store=root.findViewById(R.id.storeName);
-			store.setText("商店名稱:"+storeName);
+				@Override
+				public void onCancelled(@NonNull DatabaseError error) {
+
+				}
+			});
+		cartModelList = new ArrayList<>();
+		cartAdapter = new MyCartAdapter(getActivity(),cartModelList);
+
+		cartModelList2 = new ArrayList<>();
+		cartAdapter2 = new MyCartAdapter(getActivity(),cartModelList2);
+
+		cartModelList3 = new ArrayList<>();
+		cartAdapter3 = new MyCartAdapter(getActivity(),cartModelList3);
+
+		recyclerView = root.findViewById(R.id.store_recyclerview);
+		recyclerView2 = root.findViewById(R.id.store_recyclerview2);
+		recyclerView3 = root.findViewById(R.id.store_recyclerview3);
+
+		recyclerView.setVisibility(View.GONE);
+		recyclerView2.setVisibility(View.GONE);
+		recyclerView3.setVisibility(View.GONE);
+
+		recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),  RecyclerView.HORIZONTAL, false));
+		recyclerView2.setLayoutManager(new LinearLayoutManager(getActivity(),  RecyclerView.HORIZONTAL, false));
+		recyclerView3.setLayoutManager(new LinearLayoutManager(getActivity(),  RecyclerView.HORIZONTAL, false));
+
+
+
+		recyclerView.setAdapter(cartAdapter);
+		recyclerView2.setAdapter(cartAdapter2);
+		recyclerView3.setAdapter(cartAdapter3);
+
+
+		DatabaseReference ordersRef = database.getReference("/Orders/");
 
 			ordersRef.addValueEventListener(new ValueEventListener() {
 				@Override
 				public void onDataChange(DataSnapshot dataSnapshot) {
+					cartModelList.add(new MyCartModel(1,"store222", "customer", "delivery", 1,"remark","payment","address"));
+					cartModelList.add(new MyCartModel(1,"store222", "customer", "delivery", 1,"remark","payment","address"));
+					cartModelList.add(new MyCartModel(1,"store222", "customer", "delivery", 1,"remark","payment","address"));
+					cartModelList2.add(new MyCartModel(3,"store444", "customer", "delivery", 1,"remark","payment","address"));
+					cartModelList3.add(new MyCartModel(4,"store121", "customer", "delivery", 1,"remark","payment","address"));
+
+					cartAdapter.notifyDataSetChanged();
+					recyclerView.setVisibility(View.VISIBLE);
+					recyclerView2.setVisibility(View.VISIBLE);
+					recyclerView3.setVisibility(View.VISIBLE);
 					for (DataSnapshot ds : dataSnapshot.getChildren()) {
 						System.out.println("Data:"+ds);
 						FullOrderModel fullOrder = ds.getValue(FullOrderModel.class);
+						System.out.println(fullOrder.getStore()+"!!RRRRRR:!!"+storeName);
 						if(fullOrder.getStore().equals(storeName)){
+							System.out.println("fullOrder.getStore():"+fullOrder.getStore());
+							System.out.println("fullOrder.getType():"+fullOrder.getType());
 							switch(fullOrder.getType()){
 								case 1:
-									System.out.println("Type==1!!!!!");
-									//生成訂單按鈕
+									System.out.println("Type==1!!!!!"+fullOrder.getType());
+									break;
 								case 2:
 									System.out.println("Type==2!!!!!");
-									//生成訂單按鈕
+									break;
 								case 3:
 									System.out.println("Type==3!!!!!");
-									//生成訂單按鈕
+									break;
+								default:
+									break;
 							}
 						}
 					}
-
-
 				}
 				@Override
 				public void onCancelled(DatabaseError error) {
