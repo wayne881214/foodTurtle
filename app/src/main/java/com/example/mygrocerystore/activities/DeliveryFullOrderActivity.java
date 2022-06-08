5b7be323a6782618a0aa99d5b75e567fbe563320
peter <abcde12345326@gmail.com>
2022-06-09 00:17:40 +0800

    peter-order

---
+++ app/src/main/java/com/example/mygrocerystore/activities/DeliveryFullOrderActivity.java
@@ -0,0 +1,170 @@
+package com.example.mygrocerystore.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.adapter.OrderAdapter;
+import com.example.mygrocerystore.models.FullOrderModel;
+import com.example.mygrocerystore.models.OrderModel;
+import com.example.mygrocerystore.models.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DeliveryFullOrderActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    RecyclerView recyclerView;
+    EditText remarkEdit,paymentEdit,addressEdit;
+    Button button;
+    TextView textview,textView11,textView14,textView12;
+    OrderAdapter orderAdapter;
+    List<OrderModel> OrderModelList;
+    Toolbar toolbar;
+    ProgressBar progressBar;
+    String storeName;
+    String name;
+    int total=0;
+    int productCount=0;
+		FullOrderModel fullOrder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_order);
+
+			database=FirebaseDatabase.getInstance();
+			database.getReference().child("DeliveryMan").child(FirebaseAuth.getInstance().getUid())
+			.addListenerForSingleValueEvent(new ValueEventListener() {
+				@Override
+				public void onDataChange(@NonNull DataSnapshot snapshot) {
+					UserModel userModel = snapshot.getValue(UserModel.class);
+					OrderDetail.customer=userModel.getName();
+					name=userModel.getName();
+//					TextView DeliveryName=findViewById(R.id.DeliveryName2);
+//					DeliveryName.setText("外送員名稱:"+name);
+				}
+
+				@Override
+				public void onCancelled(@NonNull DatabaseError error) {
+
+				}
+			});
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.VISIBLE);
+        textview = findViewById(R.id.textView5);
+
+				textView11=findViewById(R.id.textView11);
+				textView14=findViewById(R.id.textView14);
+				textView12=findViewById(R.id.textView12);
+
+        remarkEdit=findViewById(R.id.remark);
+        paymentEdit=findViewById(R.id.payment);
+        addressEdit=findViewById(R.id.address);
+        button =findViewById(R.id.toOrder);
+        recyclerView = findViewById(R.id.order_rec);
+        recyclerView.setVisibility(View.GONE);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        storeName = getIntent().getStringExtra("storeName");
+        toolbar.setTitle(storeName);
+        database =FirebaseDatabase.getInstance();
+
+        OrderModelList=new ArrayList<>();
+        orderAdapter = new OrderAdapter(this, OrderModelList);
+        recyclerView.setAdapter(orderAdapter);
+
+      //先把訂單資料存下
+			DatabaseReference OrdersTypeRef = database.getReference().child("Orders").child(OrderDetail.order);
+			OrdersTypeRef.addValueEventListener(new ValueEventListener() {
+				public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+					fullOrder = dataSnapshot.getValue(FullOrderModel.class);
+					textView11.setText("備註:"+fullOrder.getRemark());
+					textView12.setText("付款方式:"+fullOrder.getPayment());
+					textView14.setText("地址"+fullOrder.getAddress());
+
+					TextView customer=(TextView) findViewById(R.id.customer);
+					customer.setText(fullOrder.getCustomer());
+					TextView deliveryMan=(TextView)findViewById(R.id.deliveryMan);
+					deliveryMan.setText(fullOrder.getDelivery());
+				}
+
+				@Override
+				public void onCancelled(DatabaseError error) {
+					// Failed to read value
+				}
+			});
+
+        DatabaseReference ProductCountRef = database.getReference("Counts/count");
+        ProductCountRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                productCount=dataSnapshot.getValue(int.class);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+        //顯示購物車中內的商品資訊
+        DatabaseReference OrdersRef = database.getReference().child("Orders").child(OrderDetail.order).child("product");
+        OrdersRef.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            		System.out.println("OrderDetail.order:"+OrderDetail.order);
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+										OrderModel order = ds.getValue(OrderModel.class);
+										total+=order.getTotalPrice();
+                    OrderModelList.add(order);
+                    orderAdapter.notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+                textview.setText("總計金額:"+total+"$");
+                button.setText("確認訂單"+" "+total+"$");
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
+        //點擊修改訂單
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+							Map<String, Object> order = new HashMap<>();
+							order.put("key",fullOrder.getKey());
+							order.put("delivery",name);
+							order.put("type",fullOrder.getType()+1);
+							database.getReference().child("Orders").child(OrderDetail.order).updateChildren(order);
+							Toast.makeText(DeliveryFullOrderActivity.this,fullOrder.getType()+"接受修改", Toast.LENGTH_LONG).show();
+//							Intent intent=new Intent();
+//							intent.setClass(StoreFullOrderActivity.this, SlideshowFragment.class);
+//							startActivity(intent);
+							finish();
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mygrocerystore/ui/DeliveryFragment2/DeliveryFragment2.java
+++ app/src/main/java/com/example/mygrocerystore/ui/DeliveryFragment2/DeliveryFragment2.java
@@ -4,20 +4,109 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.activities.OrderDetail;
+import com.example.mygrocerystore.adapter.DeliveryOrderAdapter;
+import com.example.mygrocerystore.adapter.FullOrderAdapter;
+import com.example.mygrocerystore.adapter.MyCartAdapter;
+import com.example.mygrocerystore.models.FullOrderModel;
+import com.example.mygrocerystore.models.MyCartModel;
+import com.example.mygrocerystore.models.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeliveryFragment2 extends Fragment {
-
+	FirebaseDatabase database;
+	String name="null";
+	List<MyCartModel> cartModelList,cartModelList2,cartModelList3;
+	List<FullOrderModel> fullOrderModelList,fullOrderModelList2,fullOrderModelList3;
+	MyCartAdapter cartAdapter,cartAdapter2,cartAdapter3;
+	DeliveryOrderAdapter fullOrderAdapter,fullOrderAdapter2,fullOrderAdapter3;
+	RecyclerView recyclerView,recyclerView2,recyclerView3;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+			View root = inflater.inflate(R.layout.fragment_delivery2, container, false);
 
-        View root = inflater.inflate(R.layout.fragment_delivery2, container, false);
+			database=FirebaseDatabase.getInstance();
+			database.getReference().child("DeliveryMan").child(FirebaseAuth.getInstance().getUid())
+			.addListenerForSingleValueEvent(new ValueEventListener() {
+				@Override
+				public void onDataChange(@NonNull DataSnapshot snapshot) {
+					UserModel userModel = snapshot.getValue(UserModel.class);
+					OrderDetail.customer=userModel.getName();
+					name=userModel.getName();
+					TextView DeliveryName=root.findViewById(R.id.DeliveryName2);
+					DeliveryName.setText("外送員名稱:"+name);
+				}
 
-        return root;
+				@Override
+				public void onCancelled(@NonNull DatabaseError error) {
+
+				}
+			});
+				fullOrderModelList2 = new ArrayList<>();
+				fullOrderAdapter2 = new DeliveryOrderAdapter(getActivity(),fullOrderModelList2);
+				recyclerView2 = root.findViewById(R.id.store_recyclerview2);
+				recyclerView2.setVisibility(View.GONE);
+				recyclerView2.setLayoutManager(new LinearLayoutManager(getActivity(),  RecyclerView.HORIZONTAL, false));
+				recyclerView2.setAdapter(fullOrderAdapter2);
+
+			fullOrderModelList3 = new ArrayList<>();
+			fullOrderAdapter3 = new DeliveryOrderAdapter(getActivity(),fullOrderModelList3);
+			recyclerView3 = root.findViewById(R.id.store_recyclerview3);
+			recyclerView3.setVisibility(View.GONE);
+			recyclerView3.setLayoutManager(new LinearLayoutManager(getActivity(),  RecyclerView.HORIZONTAL, false));
+			recyclerView3.setAdapter(fullOrderAdapter3);
+			FirebaseDatabase database = FirebaseDatabase.getInstance();
+			DatabaseReference ordersRef = database.getReference("/Orders/");
+
+			ordersRef.addValueEventListener(new ValueEventListener() {
+				@Override
+				public void onDataChange(DataSnapshot dataSnapshot) {
+
+					recyclerView2.setVisibility(View.VISIBLE);
+					recyclerView3.setVisibility(View.VISIBLE);
+					for (DataSnapshot ds : dataSnapshot.getChildren()) {
+						FullOrderModel fullOrder = ds.getValue(FullOrderModel.class);
+						fullOrder.showAll();
+							switch(fullOrder.getType()){
+								case 1:
+//									fullOrderModelList.add(fullOrder);
+									break;
+								case 2:
+									System.out.println("RRRRRRRR");
+									fullOrderModelList2.add(fullOrder);
+									break;
+								case 3:
+									fullOrderModelList3.add(fullOrder);
+									break;
+								default:
+									break;
+							}
+					}
+				}
+				@Override
+				public void onCancelled(DatabaseError error) {
+					// Failed to read value
+				}
+			});
+
+
+			return root;
     }
 
 }
