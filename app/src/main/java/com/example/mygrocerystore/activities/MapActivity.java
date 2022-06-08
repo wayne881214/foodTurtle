25868446f5be03e998f183c6c02592de8d794b00
wayne881214 <77848460+wayne881214@users.noreply.github.com>
2022-06-09 00:24:15 +0800

    commit

--- app/src/main/java/com/example/mygrocerystore/activities/MapActivity.java
+++ app/src/main/java/com/example/mygrocerystore/activities/MapActivity.java
@@ -1,38 +1,31 @@
-package com.example.mygrocerystore;
+package com.example.mygrocerystore.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.Display;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MapFragment extends Fragment {
+import com.example.mygrocerystore.R;
+
+public class MapActivity extends AppCompatActivity {
 
     EditText etSource, etDestination;
     Button btTrack;
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_map, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
 
-        etSource = root.findViewById(R.id.et_source);
-        etDestination = root.findViewById(R.id.et_destination);
-        btTrack = root.findViewById(R.id.bt_track);
+        etSource = findViewById(R.id.et_source);
+        etDestination = findViewById(R.id.et_destination);
+        btTrack = findViewById(R.id.bt_track);
 
         btTrack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,15 +35,12 @@ public class MapFragment extends Fragment {
                 String sDestination = etDestination.getText().toString().trim();
 
                 if (sSource.equals("") && sDestination.equals("")) {
-                    Toast.makeText(getContext(), "輸入位址", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "輸入位址", Toast.LENGTH_SHORT).show();
                 } else {
                     DisplayTrack(sSource, sDestination);
                 }
             }
         });
-
-
-        return root;
     }
 
     private void DisplayTrack(String sSource, String sDestination) {
